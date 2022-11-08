83439509fe2835b33d6a52335fa2cb4a5d81d92f
Yousefq80 <ysalharby@gmail.com>
2022-11-08 20:03:24 +0300

    all tasks are done

--- main.dart
+++ main.dart
@@ -4,6 +4,7 @@ void main() {
   List<int> numbers = [5, 3, 15, 4, 1];
   int sum = 0;
   List<int> visitors = [15, 50, 30, 20];
+  List<int> noodd = [2, 6, 4];
   numbers.forEach((number) => sum += number);
 
   print(sum);
@@ -16,10 +17,21 @@ void main() {
   print("Largest value in the list : ${largest_value}");
 
   filterVisitors(visitors, 15);
+
+  findOdds(numbers);
+
+  findOdds(noodd);
 }
 
 void filterVisitors(List<int> visitors, int minage) {
   List<int> allowed = visitors.where((visitor) => visitor > minage).toList();
 
   print(allowed);
+}
+
+void findOdds(List<int> numbers) {
+  final findodd =
+      numbers.firstWhere((number) => number % 2 != 0, orElse: () => 0);
+
+  print(findodd);
 }
